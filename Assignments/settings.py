934ed3111594582e5d8f886c72717497bb69921c
HuzaifaAnsari2806 <ansarihuzaifa2806@gmail.com>
2024-08-28 13:04:43 +0530

    ready for deployment3

--- Assignments/settings.py
+++ Assignments/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
     # },
     
     'default': dj_database_url.config(
-         default=f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+        default=f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     )
 }
 
